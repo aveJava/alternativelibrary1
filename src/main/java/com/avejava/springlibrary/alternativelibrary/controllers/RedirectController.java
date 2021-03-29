83936d0d463614aefa9f89fe03cf7ed4c998eb56
mmukhin <javatalk@mail.ru>
2021-03-29 19:37:44 +0300

    добавлены зависимости и некоторые файлы настроек для отображения web-страниц

--- src/main/java/com/avejava/springlibrary/alternativelibrary/controllers/RedirectController.java
+++ src/main/java/com/avejava/springlibrary/alternativelibrary/controllers/RedirectController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,43 +21,11 @@ import java.util.List;
 
 @Controller
 public class RedirectController {
-    private AuthorEntityRepo authorRepo;
-    private BookEntityRepo bookRepo;
-    private AuthorEntityService authorService;
-
-    public RedirectController(AuthorEntityRepo authorRepo, BookEntityRepo bookRepo, AuthorEntityService authorService) {
-        this.authorRepo = authorRepo;
-        this.bookRepo = bookRepo;
-        this.authorService = authorService;
-    }
-
-    //@GetMapping("/")
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @GetMapping("/")
+    //@RequestMapping(value = "", method = RequestMethod.GET)
     public String baseUrlRedirect(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("*********** Перешел на страртовую страницу ***********");
-        List<AuthorEntity> authorsList = authorRepo.findAll();
-        List<BookEntity> booksList = bookRepo.
-                findByNameContainingIgnoreCaseOrAuthorFioContainingIgnoreCaseOrderByName("та", "ко");
-
-        Sort sort = Sort.by(Sort.Direction.ASC, "fio");             // объект, инкапсулирующий параметры сортировки
-        PageRequest pageRequest = PageRequest.of(0, 10, sort);      // объект, инкапсулирующий настройки постраничности
-        // pageList будет содержать авторов с 1 (т.к. page=0) по 10 (т.к. size=10) отсортированных по полю fio.
-        // если бы параметр page был равен 1, тогда бы pageList содержал авторов с 11 по 20 (т.е. вторую страницу).
-        Page<AuthorEntity> pageList = authorRepo.findByFioContainingIgnoreCaseOrderByFio("та", pageRequest);
-
-        Page<AuthorEntity> authors = authorService.getAll(0, 10, "fio", Sort.Direction.DESC);
-
-        // проба добавления автора в БД
-        AuthorEntity author = new AuthorEntity();
-        author.setFio("Тест добавления автора");
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 1988);
-        cal.set(Calendar.MONTH, Calendar.JANUARY);
-        cal.set(Calendar.DAY_OF_MONTH, 1);
-        author.setBirthday(cal.getTime());
-        AuthorEntity newAuthor = authorService.save(author);
-
-        return "ok";
+        // перенаправление на страницу индекс
+        return "redirect:" + request.getRequestURL().append("index.xhtml").toString();
     }
 
 }
